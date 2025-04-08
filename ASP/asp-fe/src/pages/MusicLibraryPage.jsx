27c852005eba6359c20f0a71086be4b45b5d009c
petrenko4 <akinol841@gmail.com>
2025-04-08 15:30:35 +0200

    properly refresh library on upload

--- ASP/asp-fe/src/pages/MusicLibraryPage.jsx
+++ ASP/asp-fe/src/pages/MusicLibraryPage.jsx
@@ -1,29 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/commonStyles.css';
 import '../styles/tracklistStyles.css';
 import { getTracks } from '../services/tracksService';
-import {TrackList} from '../components/TrackList';
+import { TrackList } from '../components/TrackList';
 
 function MusicLibraryPage() {
-  const [tracks, setTracks] = useState([]);
+    const [tracks, setTracks] = useState([]);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    getTracks()
-      .then((tracks) => {
-        setTracks(tracks);
-        console.log('Tracks set:', tracks);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+    const fetchTracks = () => {
+        getTracks()
+            .then((tracks) => {
+                setTracks(tracks);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
 
-  return (
-    <div className="container mt-5">
-      <h1>My Music Library</h1>
-      <TrackList tracks={tracks}></TrackList>
-    </div>
-  );
+    useEffect(() => {
+        fetchTracks();
+    }, []);
+
+    useEffect(() => {
+        fetchTracks();
+    }, [navigate]);
+
+
+    return (
+        <div className="container mt-5">
+            <h1>My Music Library</h1>
+            <TrackList tracks={tracks}></TrackList>
+        </div>
+    );
 }
 
 export default MusicLibraryPage;
