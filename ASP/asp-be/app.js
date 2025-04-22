761ddb563e1fbb7a451a413417de3eb8c29a7724
petrenko4 <akinol841@gmail.com>
2025-04-22 14:59:46 +0200

    deployment changes

--- ASP/asp-be/app.js
+++ ASP/asp-be/app.js
@@ -15,6 +15,13 @@ const {config} = require('./config/config');
 var app = express();
 var cors = require('cors');
 
+if (process.env.STATUS === 'production') {
+    // trust proxy needed for secure cookie to work on render.com
+    // because render.com uses a reverse proxy to handle HTTPS requests
+    // and forwards the requests to the backend server over HTTP
+    app.set('trust proxy', 1);
+}
+
 app.use(
     session({
         store: new PgSession({
@@ -29,8 +36,9 @@ app.use(
             secure: process.env.STATUS === 'production',
             httpOnly: true,
             sameSite: process.env.STATUS === 'production'?'none':'lax',
-            maxAge: 1000 * 60 * 60 * 24 }            
-             
+            maxAge: 1000 * 60 * 60 * 24 
+        }            
+         
     })    
 ); 
 
